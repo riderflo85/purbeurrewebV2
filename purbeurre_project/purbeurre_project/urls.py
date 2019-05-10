b797c7029f91baecbe084d76202a9b1975974183
Florent <grenaille.florent85@gmail.com>
2019-05-10 17:55:11 +0200

    Ré organisation des fichiers static et réglage des urls et de settings.py pour afficher correctement le theme bootstrap

--- purbeurre_project/purbeurre_project/urls.py
+++ purbeurre_project/purbeurre_project/urls.py
@@ -17,7 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    path('', include('search.urls')),
     path('admin/', admin.site.urls),
 ]
 
