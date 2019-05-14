2d406254afe79e7be60d7c62bda14e3462de5715
Florent <grenaille.florent85@gmail.com>
2019-05-14 16:50:51 +0200

    Ajout de template 'base.html' et 'account.html'

--- purbeurre_project/search/urls.py
+++ purbeurre_project/search/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'search'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account', views.account, name='account'),
 ]
--- purbeurre_project/search/views.py
+++ purbeurre_project/search/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'search/index.html')
+
+def account(request):
+    return render(request, 'search/account.html')
