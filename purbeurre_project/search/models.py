b6e61b6ee1570070c550f41d3afe544eb123e152
Florent <grenaille.florent85@gmail.com>
2019-05-16 12:58:38 +0200

    Modification de 'models.py' pour ajouter les realtions entre les tables (FK)

--- purbeurre_project/search/models.py
+++ purbeurre_project/search/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Aliment(models.Model):
     name = models.CharField(max_length=140, null=False, unique=True)
-    categorie = models.CharField(max_length=45, null=False, unique=True)
     nutrition_group = models.CharField(max_length=2, verbose_name="Groupe nutritionnel")
     nova_group = models.CharField(max_length=2)
     shop = models.CharField(max_length=80, verbose_name="Boutique d'achat")
     link = models.CharField(max_length=140, verbose_name="Lien internet")
+    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "aliment"
@@ -19,20 +19,9 @@ class Categorie(models.Model):
 
     class Meta:
         verbose_name = "categorie"
-    
+
     def __str__(self):
         return self.name
-
-class Favoris(models.Model):
-    id_user = models.IntegerField(null=False)
-    favoris_aliment = models.IntegerField()
-    substitute = models.IntegerField()
-
-    class Meta:
-        verbose_name = "favoris"
-    
-    def __str__(self):
-        return self.id_user
 
 class User(models.Model):
     last_name = models.CharField(max_length=45, null=False, verbose_name="Nom d'utilisateur")
@@ -40,10 +29,21 @@ class User(models.Model):
     pseudo = models.CharField(max_length=45, null=False, unique=True)
     email = models.EmailField(max_length=80, null=False)
     password = models.CharField(max_length=88, null=False)
-    favoris = models.IntegerField()
+    favoris_save = models.ManyToManyField(Aliment, through='Favoris')
 
     class Meta:
         verbose_name = "utilisateur"
-    
+
     def __str__(self):
-        return self.last_name + " " + self.first_name
+        return "{} {}".format(self.last_name, self.first_name)
+
+class Favoris(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aliment = models.ForeignKey(Aliment, on_delete=models.CASCADE)
+    substitute = models.IntegerField()
+
+    class Meta:
+        verbose_name = "favoris"
+
+    def __str__(self):
+        return self.user
