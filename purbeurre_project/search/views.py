1ffc22a7684290153bb5a71e5b389344cae7ab45
Florent <grenaille.florent85@gmail.com>
2019-05-23 01:13:49 +0200

    Configuration du formulaire de recherche de la page 'index' (fonctionnel mais pas encore au top)

--- purbeurre_project/search/views.py
+++ purbeurre_project/search/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .models import Aliment
 
 # Create your views here.
 def index(request):
+
+    if request.method == 'POST':
+        food_search = request.POST.get('food')
+
+        food = Aliment.objects.filter(name__contains=food_search)
+        if food.exists():
+            context = {
+                'id': food[0].id,
+                'name': food[0].name,
+                'groupe_nova': food[0].nova_group,
+                'list_food': food,
+                }
+        
+        return render(request, 'search/result.html', context=context)
+
     return render(request, 'search/index.html')
 
 def login(request):
@@ -13,10 +31,16 @@ def sign_up(request):
 def account(request):
     return render(request, 'search/account.html')
 
-def result(request):
-    context = {
-        "aliment": "Pomme de terre",
-        "img": ["test"],
-        "testListFood": ["Patate", "Choux", "Pomme", "Poire", "Banane", "mangue", "fraise", "framboise", "Mur", "Fruit de la passion", "Pêche", "Abricot", "Grenadine", "Ananas", "Prune", "Nectarine"],
-        }
-    return render(request, 'search/result.html', context)
+# def result(request):
+    # list_food = []
+    # search = Aliment.objects.filter(name__contains=food)
+    # list_food.append(search[0])
+
+    # pour_test = ["Patate", "Choux", "Pomme", "Poire", "Banane", "mangue", "fraise", "framboise", "Mur", "Fruit de la passion", "Pêche", "Abricot", "Grenadine", "Ananas", "Prune", "Nectarine"]
+
+    # context = {
+    #     "aliment": "Aliments chercher",
+    #     "img": ["test"],
+    #     "testListFood": pour_test,
+    #     }
+    # return render(request, 'search/result.html', context)
