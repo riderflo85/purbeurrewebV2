2e26807aa0fc8c0b68caaf0e041e5c5eb68f2fe3
Florent <grenaille.florent85@gmail.com>
2019-05-27 21:07:59 +0200

    Ajout du fichier 'completed_db.py' qui permet de remplir la base de données. A revoir, peut être amélioré

---
+++ purbeurre_project/search/complete_db.py
@@ -0,0 +1,134 @@
+import requests
+import json
+import os
+import django
+
+try:
+    from .models import Aliment, Categorie
+except ModuleNotFoundError:
+    pass 
+
+
+def pull_data(categ):
+    page = 1
+    api = "https://fr.openfoodfacts.org/categorie/{}/{}".format(categ, page)
+    payload = {"json": 1}
+    response = requests.get(url=api, params=payload)
+    try:
+        return response.json()
+    except json.decoder.JSONDecodeError:
+        pass
+
+def table_categorie(categ):
+    try:
+        for i in categ:
+            #--- remplissage de la table catégorie de la BDD ---#
+            new_cat = Categorie()
+            new_cat.name = i
+            new_cat.save()
+            #---------------------------------------------------#
+        return True
+    except:
+        return False
+
+def table_aliment(cat, dico_food):
+    try:
+        id_cat = 1
+        for i in cat:
+            for food in dico_food[i]:
+                new_food = Aliment()
+                new_food.name = food[0]
+                new_food.nutrition_group = food[1]
+                new_food.nova_group = food[2]
+                new_food.shop = food[3]
+                new_food.image = food[4]
+                new_food.link = food[5]
+                new_food.categorie_id = id_cat
+                new_food.save()
+            id_cat += 1
+            return True
+    except:
+        return False
+
+def delete_duplicates(categorie, dico_not_sorted):
+    """ Data Sorting Function. Removes duplicates from a json file"""
+
+    list_sort = []
+    temp = []
+    all_temp = []
+    dico_sorted = {}
+
+    # Route every categories of food
+    for cat in categorie:
+
+        for food_list in dico_not_sorted[cat]:
+            # Each food in the category is added to the sort list
+            list_sort.append(food_list)
+
+        index = -1
+
+        # Pathways the food list that may potentially contain duplicates
+        for x in list_sort[:]:
+            index += 1
+
+            # If the current food is not on the temporary list it is added
+            if x[0].lower() not in temp and x[0].lower() not in all_temp:
+                temp.append(x[0].lower())
+                all_temp.append(x[0].lower())
+
+            # If the current food is already in the temporary list it
+            # means that the current food is a duplicate, so we delete
+            # it from its list thanks to its index
+            else:
+                del(list_sort[index])
+                index -= 1
+
+        # Add the name of the current category and all its foods sorted
+        # in a dictionary
+        dico_sorted[cat] = list_sort
+
+        # Empty the contents of both lists to be able to repeat the same
+        # operation for the next category
+        list_sort = []
+        temp = []
+
+    all_temp = []
+
+    return dico_sorted
+
+def main():
+    cat = [
+        "boissons", "cereales-et-derives", "desserts", "fruits",
+        "legumes-et-derives", "poissons", "produits-laitiers", "viandes"
+        ]
+
+    dico = {}
+    list_test = []
+
+    for i in cat:
+        rep = pull_data(i)
+
+        for x in rep['products']:
+            try:
+                pn = x["product_name_fr"].replace("\n", " ")
+                ng = x["nutrition_grade_fr"]
+                nova = x["nova_groups"]
+                st = str(x["stores_tags"]).replace("[", "")
+                st = st.replace("]", "")
+                st = st.replace("'", "")
+                img = x["image_url"]
+                url = x["url"]
+                if pn!="" and ng!="" and nova!="" and st!="" and img!="" and url!="":
+                    list_test.append([pn, ng, nova, st, img, url])
+                    dico[i] = list_test
+            except:
+                pass
+        list_test = []
+
+    sort = delete_duplicates(cat, dico)
+
+    with open('result_test.json', 'w') as file:
+        json.dump(sort, file, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    main()
--- purbeurre_project/search/models.py
+++ purbeurre_project/search/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Aliment(models.Model):
     name = models.CharField(max_length=140, null=False, unique=True)
     nutrition_group = models.CharField(max_length=1, verbose_name="Groupe nutritionnel")
-    nova_group = models.CharField(max_length=1)
+    nova_group = models.IntegerField()
     shop = models.CharField(max_length=80, verbose_name="Boutique d'achat")
+    image = models.CharField(max_length=240)
     link = models.CharField(max_length=140, verbose_name="Lien internet")
     categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
 
