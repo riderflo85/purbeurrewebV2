3d0922893ded36d00c2f0a75f1dc04c23eb3c616
Florent <grenaille.florent85@gmail.com>
2019-05-31 10:49:59 +0200

    Création du premier test

--- purbeurre_project/search/tests.py
+++ purbeurre_project/search/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class SignInTestCase(TestCase):
+    def setUp(self):
+        user_test = User.objects.create_user(username='testUser', email='testuser@founisseur.com', password='test')
+        user_test.first_name = 'Tester'
+        user_test.last_name = 'FooTest'
+        user_test.save()
+    
+    
