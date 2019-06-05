843327388b50559428992306f345ade5ab4a2021
Florent <grenaille.florent85@gmail.com>
2019-06-05 16:23:07 +0200

    Ajout d'une varaible d'environnement

--- purbeurre_project/purbeurre_project/settings.py
+++ purbeurre_project/purbeurre_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oke7xlhg_wuf4vrc=1tc#+imjbqmfj&=t&^ni=xk3ge@@3kg&i'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
