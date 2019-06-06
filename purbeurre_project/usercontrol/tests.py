b1ca35b6925dc08e0a6956be63f65a05b26b319f
Florent <grenaille.florent85@gmail.com>
2019-06-06 15:04:03 +0200

    Ajout de test

--- purbeurre_project/usercontrol/tests.py
+++ purbeurre_project/usercontrol/tests.py
@@ -21,7 +21,7 @@ class StatusCodePageTestCase(TestCase):
         rep = self.cli.get('/user/account')
         self.assertEqual(rep.status_code, 302)
 
-class SignInTestCase(TestCase):
+class UserAuthenticateTestCase(TestCase):
     def setUp(self):
         self.cli = Client()
         user_test = User.objects.create_user(username='testUser', email='testuser@founisseur.com', password='test')
@@ -29,11 +29,31 @@ class SignInTestCase(TestCase):
         user_test.last_name = 'FooTest'
         user_test.save()
 
+    def test_user_sign_in(self):
+        rep = self.cli.get('/user/signin')
+        self.assertEqual(rep.status_code, 200)
+
     def test_user_redirect_after_sign_in(self):
         rep = self.cli.post('/user/signin', {'user': 'testUser', 'password': 'test'})
         self.assertEqual(rep.status_code, 302)
-    
-    def test_user_sign_in(self):
-        rep = self.cli.get('/user/signin')
-        print(rep)
-        self.assertEqual(rep.status_code, 200)
+
+    def test_user_is_authenticated(self):
+        rep = self.cli.login(username='testUser', password='test')
+        self.assertEqual(rep, True)
+
+        rep2 = self.cli.get('/user/signin')
+        self.assertEqual(rep2.context['user'].get_username(), 'testUser')
+
+
+class CreateNewUserTestCase(TestCase):
+    def test_sign_up(self):
+        cli = Client()
+        rep = cli.post('/user/signup', {
+            'pseudo': 'testUser',
+            'last_name': 'FooTest',
+            'first_name': 'Tester',
+            'email': 'testuser@founisseur.com',
+            'password': 'test'
+            })
+        
+        print(rep.context['user'])
