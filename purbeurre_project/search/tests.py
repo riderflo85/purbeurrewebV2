efcc5a329373de0192f4577b4ee3934cef9968fb
Florent <grenaille.florent85@gmail.com>
2019-06-13 18:23:55 +0200

    Ajout d'un test pour la page de détail d'un produit

--- purbeurre_project/search/tests.py
+++ purbeurre_project/search/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase, Client
+from .models import Categorie, Aliment
 
     
 class SatusCodePageTestCase(TestCase):
     def setUp(self):
         self.cli = Client()
+        cat = Categorie()
+        cat.name = 'boisson'
+        cat.save()
+        alim = Aliment()
+        alim.name = 'Ice Tea'
+        alim.nutrition_group = 'a'
+        alim.nova_group = 1
+        alim.shop = 'Hyper U'
+        alim.link = 'https://link.shop.com'
+        alim.nutriments = "{'succre pour 100g': 12}"
+        alim.categorie = cat
+        alim.save()
 
     def test_page_index(self):
         rep = self.cli.get('/')
@@ -11,4 +24,8 @@ class SatusCodePageTestCase(TestCase):
 
     def test_page_result(self):
         rep = self.cli.get('/result')
+        self.assertEqual(rep.status_code, 200)
+
+    def test_page_food_detail(self):
+        rep = self.cli.get('/food_detail/1')
         self.assertEqual(rep.status_code, 200)
