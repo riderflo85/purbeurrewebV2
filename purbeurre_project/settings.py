95448428661e3b776a32f0c43f086db5170aa8ac
Florent <grenaille.florent85@gmail.com>
2019-06-20 11:59:42 +0200

    Suppression de la debug_toolbar

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 if os.environ.get('ENV') == 'PRODUCTION':
