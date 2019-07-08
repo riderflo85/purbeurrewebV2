c3d48cb684caeb2ab156ce0bf2cd561b2b65727d
Florent <grenaille.florent85@gmail.com>
2019-07-08 19:34:19 +0200

    Ajout de tests

--- usercontrol/tests.py
+++ usercontrol/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from .forms import SignupForm, LoginForm
+from .views import sign_in, sign_out, sign_up, account
 
 
 class StatusCodePageTestCase(TestCase):
@@ -52,6 +53,10 @@ class UserAuthenticateTestCase(TestCase):
 
         rep2 = self.cli.get('/user/signin')
         self.assertEqual(rep2.context['user'].get_username(), 'testUser')
+
+    def test_user_is_not_authenticated(self):
+        rep = self.cli.login(username='notUser', password='notPwd')
+        self.assertFalse(rep)
     
     def test_signup(self):
         user_test2 = User.objects.create_user(
@@ -85,3 +90,45 @@ class FormTestCase(TestCase):
         }
         form = LoginForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+
+class TemplateRenderTestCase(TestCase):
+    def setUp(self):
+        self.cli = Client()
+
+    def test_template_page_sign_in(self):
+        rep = self.cli.get('/sign_in')
+        self.assertTemplateUsed('/user/login.html')
+
+    def test_template_page_sign_up(self):
+        rep = self.cli.get('/sign_up')
+        self.assertTemplateUsed('/user/sign_up.html')
+
+    def test_template_page_sign_out(self):
+        rep = self.cli.get('/sign_out')
+        self.assertTemplateUsed('/user/sign_out.html')
+
+    def test_template_page_account(self):
+        rep = self.cli.get('/account')
+        self.assertTemplateUsed('/user/account.html')
+
+
+class ViewsUsedTestCase(TestCase):
+    def setUp(self):
+        self.cli = Client()
+
+    def test_views_page_sing_in(self):
+        rep = self.cli.get('/user/signin')
+        self.assertEqual(rep.resolver_match.func, sign_in)
+
+    def test_views_page_sing_up(self):
+        rep = self.cli.get('/user/signup')
+        self.assertEqual(rep.resolver_match.func, sign_up)
+
+    def test_views_page_sing_out(self):
+        rep = self.cli.get('/user/signout')
+        self.assertEqual(rep.resolver_match.func, sign_out)
+
+    def test_views_page_account(self):
+        rep = self.cli.get('/user/account')
+        self.assertEqual(rep.resolver_match.func, account)
